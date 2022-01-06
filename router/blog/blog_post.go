531efb368c5933a2f9634c8582d4b94aef29f7c3
Lei Zhu <rodrickjue@hotmail.com>
2022-01-06 16:54:52 +0800

    :recycle: change blogPostRouter's base path to blog/posts

--- router/blog/blog_post.go
+++ router/blog/blog_post.go
@@ -9,10 +9,10 @@ import (
 type BlogPostRouter struct{}
 
 func (s *BlogPostRouter) InitBlogPostRouter(Router *gin.RouterGroup) {
-	blogRouter := Router.Group("blog")
+	blogPostRouter := Router.Group("blog/posts")
 	blogPostService := service.ServiceGroupApp.BlogServiceGroup.BlogPostService
 
-	blogRouter.GET("posts", blogPostService.GetBlogPosts)
-	blogRouter.GET("posts/:slug", blogPostService.GetBlogPostBySlug)
-	blogRouter.POST("posts", blogPostService.AddBlogPost)
+	blogPostRouter.GET("", blogPostService.GetBlogPosts)
+	blogPostRouter.GET(":slug", blogPostService.GetBlogPostBySlug)
+	blogPostRouter.POST("", blogPostService.AddBlogPost)
 }
