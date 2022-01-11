a29746903ab3fc5a371fc2c79edd1ac7fc60b791
Lei Zhu <rodrickjue@hotmail.com>
2022-01-11 18:29:19 +0800

    :card_file_box: change post content to text

--- model/blog/blog_post.go
+++ model/blog/blog_post.go
@@ -10,7 +10,7 @@ type BlogPost struct {
 	Title       string       `json:"title" gorm:"comment:blog title"`
 	Slug        string       `json:"slug" gorm:"comment:unique identifying part of blog post;index"`
 	CoverImg    string       `json:"coverImg" gorm:"comment:url of cover image"`
-	Content     string       `json:"content" gorm:"comment:blog content"`
+	Content     string       `json:"content" gorm:"type:text;comment:blog content"`
 	AuthorId    uint         `json:"authorId" gorm:"comment:author id;default:0"`
 	CategoryId  uint         `json:"categoryId" gorm:"comment:post category id;default:0"`
 	PublishedAt sql.NullTime `json:"publishedAt" gorm:"comment:time that post published"`
