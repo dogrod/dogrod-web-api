007cd11174d559b376d38fdd21c1ec5ce7d1deaf
Lei Zhu <rodrickjue@hotmail.com>
2022-01-13 14:24:15 +0800

    :card_file_box: rename blog_category title column to name

--- model/blog/blog_category.go
+++ model/blog/blog_category.go
@@ -4,6 +4,6 @@ import "dogrod-web-service/global"
 
 type BlogCategory struct {
 	global.BaseModel
-	Title string `json:"title" gorm:"comment:category title"`
-	Slug  string `json:"slug" gorm:"comment:unique identifying part of blog category"`
+	Name string `json:"title" gorm:"comment:category title"`
+	Slug string `json:"slug" gorm:"comment:unique identifying part of blog category"`
 }
