3040b5bb2cc1a399957f2025ee6407de4cddbf7d
Lei Zhu <rodrickjue@hotmail.com>
2022-01-13 14:24:32 +0800

    :card_file_box: change type of published_at to time.Time

--- model/blog/blog_post.go
+++ model/blog/blog_post.go
@@ -1,17 +1,19 @@
 package blog
 
 import (
-	"database/sql"
 	"dogrod-web-service/global"
+	"time"
 )
 
 type BlogPost struct {
 	global.BaseModel
-	Title       string       `json:"title" gorm:"comment:blog title"`
-	Slug        string       `json:"slug" gorm:"comment:unique identifying part of blog post;index"`
-	CoverImg    string       `json:"coverImg" gorm:"comment:url of cover image"`
-	Content     string       `json:"content" gorm:"type:text;comment:blog content"`
-	AuthorId    uint         `json:"authorId" gorm:"comment:author id;default:0"`
-	CategoryId  uint         `json:"categoryId" gorm:"comment:post category id;default:0"`
-	PublishedAt sql.NullTime `json:"publishedAt" gorm:"comment:time that post published"`
+	Title       string     `json:"title" gorm:"comment:blog title"`
+	Slug        string     `json:"slug" gorm:"comment:unique identifying part of blog post;index"`
+	CoverImg    string     `json:"coverImg" gorm:"comment:url of cover image"`
+	Summary     string     `json:"summary" gorm:"comment:summary of post content"`
+	Content     string     `json:"content" gorm:"type:text;comment:blog content"`
+	Status      string     `json:"status" gorm:"comment:draft/publish/private/trash;default:draft"`
+	AuthorId    uint       `json:"authorId" gorm:"comment:author id;default:0"`
+	CategoryId  uint       `json:"categoryId" gorm:"comment:post category id;default:0"`
+	PublishedAt *time.Time `json:"publishedAt" gorm:"comment:time that post published"`
 }
