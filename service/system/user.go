f0b66e669678b0fc0e58558f838121d6d7b357a3
Lei Zhu <rodrickjue@hotmail.com>
2022-01-14 17:11:49 +0800

    :sparkles: user login in method

--- service/system/user.go
+++ service/system/user.go
@@ -24,3 +24,13 @@ func (systemUserService *SystemUserService) SignUp(u system.SystemUser) (err err
 	err = global.ConnectDatabase().Create(&u).Error
 	return err, u
 }
+
+// login
+func (systemUserService *SystemUserService) Login(u *system.SystemUser) (err error, userInter *system.SystemUser) {
+	var user system.SystemUser
+	u.Password = utils.MD5V([]byte(u.Password))
+
+	err = global.ConnectDatabase().Where("username = ? AND password = ?", u.Username, u.Password).Preload("Authorities").Preload("Authority").First(&user).Error
+
+	return err, &user
+}
